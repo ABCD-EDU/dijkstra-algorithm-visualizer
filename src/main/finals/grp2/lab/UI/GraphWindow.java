dc639977d82462df0a63a249a14a7f3ff437b20c
2205437 <2205437@slu.edu.ph>
2020-12-06 17:07:41 +0800

    add: promptFileSelection (no functionality) feat: layout adjustments, look & feel feat: centered program window

--- src/main/finals/grp2/lab/UI/GraphWindow.java
+++ src/main/finals/grp2/lab/UI/GraphWindow.java
@@ -1,9 +1,19 @@
 package main.finals.grp2.lab.UI;
 
+import javax.sound.sampled.Line;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+// COMMITS: WINDOW CENTERED, LAYOUT FIXES, PROMPT FILE SELECTION (NO FUNCTIONALITY)
+
+
 
 /**
  * Main structure:
@@ -17,10 +27,12 @@ import java.awt.*;
  **/
 public class GraphWindow {
 
+    protected JFrame mainFrame;
+
     // main panel for all components
     protected JPanel mainPanel, titleBarPanel;
 
-    // subpanels for controls and visualizer
+    // sub-panels for controls and visualizer
     protected JPanel controlPanel, visualPanel;
 
     // sub panels for controlPanel;
@@ -38,6 +50,31 @@ public class GraphWindow {
             new JButton(">"),
             new JButton(">>"),
     };
+
+    protected JLabel inputLabel, pathwayLabel, algorithmLabel;
+
+    protected JPanel inputMainPanel, pathwayMainPanel;
+
+    protected JPanel inputLabelPanel, pathwayLabelPanel;
+
+    protected JTable inputTable, pathwayTable;
+
+    protected JScrollPane inputTableScrollPane, pathwayTableScrollPane;
+
+
+    protected JPanel algoLabelPanel, algoSelectionPanel, playPanel, stepPanel;
+
+    protected JComboBox<String> algoSelectionBox;
+
+    protected File textFile;
+
+    // THEME: Bento
+    protected Color mainColor = new Color(0x2D394D);
+    protected Color secondaryColor = new Color(0x4A768D);
+    protected Color accentColor = new Color(0xF87A90);
+    protected Color mainForeground = Color.WHITE;
+    protected Color secondaryForeground = Color.BLACK;
+
 
 
     public GraphWindow() {
@@ -63,6 +100,8 @@ public class GraphWindow {
         // init main panel after creating all needed components
         initMainPanel();
 
+        initTheme();
+
         // only load the mainframe after inserting all components
         initMainFrame();
 
@@ -73,7 +112,7 @@ public class GraphWindow {
      * Add the component to its corresponding panel instead.
      */
     protected void initMainFrame() {
-        JFrame mainFrame = new JFrame();
+        mainFrame = new JFrame();
 
         mainFrame.add(mainPanel);
 
@@ -82,9 +121,74 @@ public class GraphWindow {
 //        mainFrame.setUndecorated(true);
         mainFrame.pack();
         mainFrame.setResizable(false);
-        mainFrame.setTitle("Graph");
-        mainFrame.setSize(new Dimension(1366, 768));
+        mainFrame.setTitle("Graph Visualizer");
+        mainFrame.setSize(new Dimension(1500, 800));
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        mainFrame.setLocation((d.width / 2) - mainFrame.getWidth() / 2, (d.height / 2) - mainFrame.getHeight() / 2);
         mainFrame.setVisible(true);
+    }
+
+    protected void initTheme() {
+        setBackgrounds();
+        setForegrounds();
+        setButtons();
+    }
+
+    protected void setBackgrounds() {
+        controlPanel.setBackground(mainColor);
+        visualPanel.setBackground(secondaryColor);
+
+        inputPanel.setBackground(mainColor);
+        tablePanel.setBackground(mainColor);
+        actionPanel.setBackground(mainColor);
+
+        inputMainPanel.setBackground(mainColor);
+        pathwayMainPanel.setBackground(mainColor);
+
+        inputTablePanel.setBackground(mainColor);
+        pathwayTablePanel.setBackground(mainColor);
+
+        inputLabelPanel.setBackground(mainColor);
+        pathwayLabelPanel.setBackground(mainColor);
+
+        algoLabelPanel.setBackground(mainColor);
+        algoSelectionPanel.setBackground(mainColor);
+        playPanel.setBackground(mainColor);
+        stepPanel.setBackground(mainColor);
+
+        inputTable.setBackground(secondaryColor);
+        inputTable.getTableHeader().setBackground(accentColor);
+        pathwayTable.setBackground(secondaryColor);
+        pathwayTable.getTableHeader().setBackground(accentColor);
+
+        algoSelectionBox.setBackground(secondaryColor);
+    }
+
+    protected void setForegrounds() {
+        inputLabel.setForeground(mainForeground);
+        pathwayLabel.setForeground(mainForeground);
+        algorithmLabel.setForeground(mainForeground);
+
+        inputTable.getTableHeader().setForeground(mainForeground);
+        pathwayTable.getTableHeader().setForeground(mainForeground);
+
+        algoSelectionBox.setForeground(mainForeground);
+
+    }
+
+    protected void setButtons() {
+        inputFileButton.setBackground(accentColor);
+        inputFileButton.setForeground(mainForeground);
+        inputFileButton.setFocusPainted(false);
+        inputFileButton.setBorder(new EmptyBorder(5, 0, 5, 0));
+
+        for (JButton button : actionButtons) {
+            button.setBackground(secondaryColor);
+            button.setForeground(mainForeground);
+            button.setFocusPainted(false);
+            button.setBorder(new EmptyBorder(0, 0, 0, 0));
+        }
+        actionButtons[0].setBackground(accentColor);
     }
 
     protected void initMainPanel() {
@@ -128,7 +232,8 @@ public class GraphWindow {
     protected void initControlPanel() {
         controlPanel = new JPanel();
         controlPanel.setLayout(new BorderLayout());
-        controlPanel.setPreferredSize(new Dimension(366, 768)); // makes the control panel smaller
+        controlPanel.setBorder(new EmptyBorder(35, 35, 60, 35));
+        controlPanel.setPreferredSize(new Dimension(400, 800)); // makes the control panel smaller
 
         controlPanel.add(inputPanel, BorderLayout.NORTH);
         controlPanel.add(tablePanel, BorderLayout.CENTER);
@@ -138,13 +243,13 @@ public class GraphWindow {
     // TODO: INSERT VISUALIZER HERE
     protected void initVisualPanel() {
         visualPanel = new JPanel();
-        visualPanel.setPreferredSize(new Dimension(1000, 768)); // makes the visual panel wider than controls
+        visualPanel.setPreferredSize(new Dimension(1100, 800)); // makes the visual panel wider than controls
     }
 
     // TODO: FIX FORMATTING
     protected void initInputPanel() {
         inputPanel = new JPanel();
-        inputPanel.setBorder(new EmptyBorder(10,10,0,10));
+        inputPanel.setBorder(new EmptyBorder(0,100,0,0));
         inputPanel.setLayout(new GridLayout(1, 2));
 
         JLabel text = new JLabel();
@@ -152,6 +257,7 @@ public class GraphWindow {
 
         // components in input panel
         inputFileButton = new JButton("Browse");
+        inputFileButton.addActionListener((e) -> promptFileSelection());
 
         inputPanel.add(text);
         inputPanel.add(inputFileButton);
@@ -162,37 +268,33 @@ public class GraphWindow {
         tablePanel = new JPanel();
         tablePanel.setLayout(new GridLayout(2,1));
 
-        final JPanel inputLabelPanel = new JPanel(new GridLayout(1,1));
-        inputLabelPanel.setBorder(new EmptyBorder(0,10,0,10));
-        final JPanel pathwayLabelPanel = new JPanel(new GridLayout(1,1));
-        pathwayLabelPanel.setBorder(new EmptyBorder(0,10,0,10));
-
-        final JLabel inputLabel = new JLabel();
-        inputLabel.setText("Input Table:");
+        inputLabelPanel = new JPanel(new GridLayout(1, 1));
+        inputLabelPanel.setBorder(new EmptyBorder(0,10,0,0));
+        inputLabel = new JLabel("Input Table");
         inputLabel.setHorizontalAlignment(SwingConstants.LEFT);
-
-        final JLabel pathwayLabel = new JLabel();
-        pathwayLabel.setHorizontalAlignment(SwingConstants.LEFT);
-        pathwayLabel.setText("Pathway Table");
-
-        // add label to own panel
         inputLabelPanel.add(inputLabel);
+
+        pathwayLabelPanel = new JPanel(new GridLayout(1, 1));
+        pathwayLabelPanel.setBorder(new EmptyBorder(0,10,0,0));
+        pathwayLabel = new JLabel("Pathway Table");
+        pathwayLabel.setHorizontalAlignment(SwingConstants.LEFT);
         pathwayLabelPanel.add(pathwayLabel);
 
-        final JPanel inputPanel = new JPanel();
-        inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
-        final JPanel outputPanel = new JPanel();
-        outputPanel.setLayout(new BoxLayout(outputPanel, BoxLayout.Y_AXIS));
+        inputMainPanel = new JPanel();
+        pathwayMainPanel = new JPanel();
+
+        inputMainPanel.setLayout(new BoxLayout(inputMainPanel, BoxLayout.Y_AXIS));
+        pathwayMainPanel.setLayout(new BoxLayout(pathwayMainPanel, BoxLayout.Y_AXIS));
 
         // add label panel and table panel
-        inputPanel.add(inputLabelPanel);
-        inputPanel.add(inputTablePanel);
+        inputMainPanel.add(inputLabelPanel);
+        inputMainPanel.add(inputTablePanel);
 
-        outputPanel.add(pathwayLabelPanel);
-        outputPanel.add(pathwayTablePanel);
+        pathwayMainPanel.add(pathwayLabelPanel);
+        pathwayMainPanel.add(pathwayTablePanel);
 
-        tablePanel.add(inputPanel);
-        tablePanel.add(outputPanel);
+        tablePanel.add(inputMainPanel);
+        tablePanel.add(pathwayMainPanel);
     }
 
     protected void initializeTable(JTable table, DefaultTableModel defaultTableModel, String[] columnNames,
@@ -213,12 +315,15 @@ public class GraphWindow {
 
     protected void initInputTablePanel() {
         inputTablePanel = new JPanel();
-        inputTablePanel.setBorder(new EmptyBorder(10,10,10,10));
+        inputTablePanel.setBorder(new EmptyBorder(5,0,10,0));
         inputTablePanel.setLayout(new GridLayout(1, 1));
 
         DefaultTableModel inputTableModel = new DefaultTableModel();
-        JTable inputTable = new JTable(inputTableModel);
-        JScrollPane inputTableScrollPane = new JScrollPane(inputTable);
+        inputTable = new JTable(inputTableModel);
+        inputTable.getTableHeader().setFont(new Font("Default", Font.PLAIN, 11));
+        inputTable.getTableHeader().setBorder(new LineBorder(accentColor, 1));
+        inputTableScrollPane = new JScrollPane(inputTable);
+        inputTableScrollPane.setBorder(new EmptyBorder(0 ,0 ,0 ,0));
 
         initializeTable(inputTable, inputTableModel, new String[]{"Weight", "Point A", "Point B"}, inputTableScrollPane);
 
@@ -227,12 +332,15 @@ public class GraphWindow {
 
     protected void initPathwayTablePanel() {
         pathwayTablePanel = new JPanel();
-        pathwayTablePanel.setBorder(new EmptyBorder(10,10,10,10));
+        pathwayTablePanel.setBorder(new EmptyBorder(5,0,10,0));
         pathwayTablePanel.setLayout(new GridLayout(1, 1));
 
         DefaultTableModel pathwayTableModel = new DefaultTableModel();
-        JTable pathwayTable = new JTable(pathwayTableModel);
-        JScrollPane pathwayTableScrollPane = new JScrollPane(pathwayTable);
+        pathwayTable = new JTable(pathwayTableModel);
+        pathwayTable.getTableHeader().setFont(new Font("Default", Font.PLAIN, 11));
+        pathwayTable.getTableHeader().setBorder(new LineBorder(accentColor, 1));
+        pathwayTableScrollPane = new JScrollPane(pathwayTable);
+        pathwayTableScrollPane.setBorder(new EmptyBorder(0 ,0 ,0 ,0));
 
         initializeTable(pathwayTable, pathwayTableModel, new String[]{"Weight", "Point fA", "Point fB"}, pathwayTableScrollPane);
 
@@ -241,22 +349,29 @@ public class GraphWindow {
 
     protected void initActionPanel() {
         actionPanel = new JPanel();
-        actionPanel.setLayout(new GridLayout(3, 1));
+        actionPanel.setLayout(new GridLayout(4, 1));
 
-        JPanel algoSelectionPanel = new JPanel(); // for the dropdown list
-        algoSelectionPanel.setBorder(new EmptyBorder(0,10,0,10));
+        algoLabelPanel = new JPanel(new GridLayout(1, 1));
+        algoSelectionPanel = new JPanel(new GridLayout(1, 1));
+        playPanel = new JPanel(new GridLayout(1, 1));
+        stepPanel = new JPanel(new GridLayout(1, 4, 5, 5));
 
-        JPanel playPanel = new JPanel(); // to occupy all space
-        JPanel stepPanel = new JPanel(); // to split the space into two
+        algoLabelPanel.setPreferredSize(new Dimension(100, 0));
+        algoSelectionPanel.setPreferredSize(new Dimension(100, 20   ));
+        playPanel.setPreferredSize(new Dimension(100, 40));
+        stepPanel.setPreferredSize(new Dimension(100, 40));
 
-        playPanel.setBorder(new EmptyBorder(10,10,0,10));
-        stepPanel.setBorder(new EmptyBorder(10,10,10,10));
+        algoLabelPanel.setBorder(new EmptyBorder(10, 30, 0, 30));
+        algoSelectionPanel.setBorder(new EmptyBorder(0,30,5,30));
+        playPanel.setBorder(new EmptyBorder(0,30,5,30));
+        stepPanel.setBorder(new EmptyBorder(0,30,5,30));
 
-        stepPanel.setLayout(new GridLayout(1, 4,3,3));
-        playPanel.setLayout(new GridLayout(1, 1));
-        algoSelectionPanel.setLayout(new GridLayout(1, 1));
+        algorithmLabel = new JLabel("Algorithm");
+        algorithmLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        algoLabelPanel.add(algorithmLabel);
 
-        JComboBox<String> algoSelectionBox = new JComboBox<>(); // Dropdown list
+        algoSelectionBox = new JComboBox<>(); // Dropdown list
+        algoSelectionBox.setFocusable(false);
         algoSelectionBox.addItem("None");
         algoSelectionBox.addItem("Depth First Search");
         algoSelectionBox.addItem("Breadth First Search");
@@ -272,6 +387,7 @@ public class GraphWindow {
         stepPanel.add(actionButtons[4]); // Go to end Button
 
         // top: dropdown list, middle: wide play/pause button, bottom: steppers
+        actionPanel.add(algoLabelPanel);
         actionPanel.add(algoSelectionPanel);
         actionPanel.add(playPanel);
         actionPanel.add(stepPanel);
@@ -289,6 +405,39 @@ public class GraphWindow {
         actionButtons[0].addActionListener(e -> {
             changeMode(mode);
         });
+    }
+
+    private void promptFileSelection() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("*.txt", "txt"));
+        int choice = fileChooser.showOpenDialog(mainFrame);
+        if (choice == JFileChooser.APPROVE_OPTION) {
+            textFile = fileChooser.getSelectedFile();
+            readLinesFromFile();
+            System.out.println("CHOSEN: " + textFile.getName());
+        } else {
+            System.out.println("File Selection Aborted");
+        }
+    }
+
+    // TODO: Transfer file info to input table
+    private void readLinesFromFile() {
+        String content = "";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(textFile.getPath()));
+            StringBuilder string = new StringBuilder();
+            String line = reader.readLine();
+            while (line != null) {
+                string.append(line);
+                string.append(System.lineSeparator());
+                line = reader.readLine();
+            }
+            content = string.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     // TODO: play pause action
